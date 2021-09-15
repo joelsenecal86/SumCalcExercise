cc42bc7a084f1defcac192d0fef43cdd4d4d54ab
Joel <joel.senecal@gmail.com>
2021-09-15 15:11:05 -0500

    Optimized Algorithm

--- SumCalcExercise.cpp
+++ SumCalcExercise.cpp
@@ -12,41 +12,27 @@ double sum(double a, double b)
 int main()
 {
     double a;
-    double b;
-    bool loop = true;
+    double b;   
 
-    do
-    {
-        if (std::cin.fail() == 0)
+    do    
+    {        
+        std::cout << "Enter Integer A: ";
+        std::cin >> a;
+
+        std::cout << "Enter Ingeter B: ";
+        std::cin >> b;
+
+        if (!std::cin)
         {
-
-            std::cout << "Enter Input A: ";
-            if (std::cin >> a)
-            {
-                std::cout << "A CAPTURED" << std::endl;                
-            }
-
-            std::cout << "Enter Input B: ";
-            if (std::cin >> b)
-            {
-                std::cout << "B CAPTURED" << std::endl;
-                loop = false;
-            }
-            else
-            {
-                std::cin.clear();
-                std::cin.ignore(10000, '\n');
-                std::cout << "Please Enter Valid Input..." << std::endl;              
-            }
+            std::cout << "Invalid Input!" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
         }
         else
         {
-            std::cin.clear();
-            std::cout << "LOWER" << std::endl;
+            break;
         }
-
-        
-    } while (loop == true);
+    }while (std::cin.bad() == false);
     
     std::cout << "The sum of " << a << " and " << b << " is: " <<  sum(a, b) << std::endl;
 }
