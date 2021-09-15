ec1b3a90c5b22a19b68a71408219880c1f62227f
Joel <joel.senecal@gmail.com>
2021-09-15 12:25:30 -0500

    rehauled logic

--- SumCalcExercise.cpp
+++ SumCalcExercise.cpp
@@ -13,14 +13,40 @@ int main()
 {
     double a;
     double b;
+    bool loop = true;
 
-    std::cout << "Enter First Number: ";
-    std::cin >> a;
-    std::cout << "The First Number is: " << a << std::endl;
+    do
+    {
+        if (std::cin.fail() == 0)
+        {
 
-    std::cout << "Enter Second Number: ";
-    std::cin >> b;
-    std::cout << "The Second Number is: " << b << std::endl;
+            std::cout << "Enter Input A: ";
+            if (std::cin >> a)
+            {
+                std::cout << "A CAPTURED" << std::endl;                
+            }
 
+            std::cout << "Enter Input B: ";
+            if (std::cin >> b)
+            {
+                std::cout << "B CAPTURED" << std::endl;
+                loop = false;
+            }
+            else
+            {
+                std::cin.clear();
+                std::cin.ignore(10000, '\n');
+                std::cout << "Please Enter Valid Input..." << std::endl;              
+            }
+        }
+        else
+        {
+            std::cin.clear();
+            std::cout << "LOWER" << std::endl;
+        }
+
+        
+    } while (loop == true);
+    
     std::cout << "The sum of " << a << " and " << b << " is: " <<  sum(a, b) << std::endl;
 }
